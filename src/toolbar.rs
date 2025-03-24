a2f0406dde7ef963a91a34535285a442460e1e59
breadam <cagatay@onclick.com.tr>
2025-03-24 04:49:06 +0300

    fnly

--- src/toolbar.rs
+++ src/toolbar.rs
@@ -2,7 +2,7 @@ use leptos::prelude::*;
 
 #[component]
 pub fn Toolbar() -> impl IntoView {
-    view! { cx,
+    view! {
         <header style="background: #222; color: white; padding: 1rem;">
             <h1>"Test"</h1>
         </header>
